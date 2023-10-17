4f5c243db8d584273a4a886bca1ef3b403964c5a
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2023-10-17 09:32:21 +0200

    ++

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField]
-    LayerMask groundlayer;
+    LayerMask groundLayer;
 
     [SerializeField]
     int speed = 5;
@@ -23,12 +23,21 @@ public class playerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         Vector2 movement = new Vector2(moveX, 0) * speed;
         transform.Translate(movement);
-        bool isGrounded()
+        bool isGrounded = Physics2D.OverlapBox(GetFootPosition(), GetFootSize(), 0, groundLayer);
+        if (isGrounded && Input.GetAxisRaw("Jump") > 0)
         {
-            if (
-                
-            )
-            return false;
+            rBody.AddForce(Vector2.up*5);
         }
     }
+
+    private Vector2 GetFootPosition()
+  {
+    float height = GetComponent<Collider2D>().bounds.size.y;
+    return transform.position + Vector3.down * height / 2;
+  }
+
+  private Vector2 GetFootSize()
+  {
+    return new Vector2(GetComponent<Collider2D>().bounds.size.x * 0.9f, 0.1f);
+  }
 }
